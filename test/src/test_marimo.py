f7aaade03de0e153d160bba9a633360544c1e9f2
kigasudayooo <argonfluorohydride1215@gmail.com>
2025-02-24 21:25:06 +0900

    rev

---
+++ test/src/test_marimo.py
@@ -0,0 +1,15 @@
+import marimo
+
+__generated_with = "0.11.8"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+
+    return (mo,)
+
+
+if __name__ == "__main__":
+    app.run()
